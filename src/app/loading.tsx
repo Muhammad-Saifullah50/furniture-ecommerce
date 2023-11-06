213cb6084bf16f0f83a5bc86dbb15de7c75bebc9
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-06 09:44:04 +0500

    corrected loadeer

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,13 @@
+'use client'
 import { RotatingLines } from "react-loader-spinner"
 const Loading = () => {
     return (
-        <section className="min-h-[75vh]">
+        <section className="min-h-[75vh] flex justify-center items-center">
             <RotatingLines
                 strokeColor="#B88E2F"
                 strokeWidth="5"
                 animationDuration="1"
-                width="96"
+                width="48"
                 visible={true}
             />
         </section>
