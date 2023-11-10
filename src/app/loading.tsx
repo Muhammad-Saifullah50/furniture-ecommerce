fa6048e139c584dc060d1afc67056adce811fe73
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-10 14:42:03 +0500

    un loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,13 +3,13 @@ import { RotatingLines } from "react-loader-spinner"
 const Loading = () => {
     return (
         <section className="min-h-[75vh] flex justify-center items-center">
-            {/* <RotatingLines
+            <RotatingLines
                 strokeColor="#B88E2F"
                 strokeWidth="5"
                 animationDuration="1"
                 width="48"
                 visible={true}
-            /> */}
+            />
         </section>
     )
 }
