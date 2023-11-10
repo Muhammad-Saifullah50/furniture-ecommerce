599602a62d5c09335e00019ba3c399534268e489
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-10 14:33:32 +0500

    pr

--- src/components/ProductRow.tsx
+++ src/components/ProductRow.tsx
@@ -19,23 +19,23 @@ interface ProductRowProps {
 
 
 const ProductRow = ({ id, name, image, createdAt, price }: ProductRowProps) => {
-    // const path = usePathname()
-    // const [loading, setLoading] = useState(false)
+    const path = usePathname()
+    const [loading, setLoading] = useState(false)
 
-    // const handleDelete = async () => {
-    //     try {
-    //         setLoading(true)
-    //         await DeleteProduct(id, path)
-    //     } catch (error) {
-    //         throw new Error('Error deleting product')
-    //     } finally {
-    //         setLoading(false)
-    //     }
-    // }
+    const handleDelete = async () => {
+        try {
+            setLoading(true)
+            await DeleteProduct(id, path)
+        } catch (error) {
+            throw new Error('Error deleting product')
+        } finally {
+            setLoading(false)
+        }
+    }
 
     return (<>
         <tr className='border-b-[1px] border-black m-2'>
-            {/* <td className='px-3'>{id}</td>
+            <td className='px-3'>{id}</td>
             <td className='px-3'><Image src={image} alt={name} width={70} height={70} className='h-16 p-1 rounded-md m-2' />
             </td>
             <td className='px-3'>{name}</td>
@@ -75,7 +75,7 @@ const ProductRow = ({ id, name, image, createdAt, price }: ProductRowProps) => {
                     />)}
 
                 </Button>
-            </td> */}
+            </td>
         </tr>
 
     </>)
