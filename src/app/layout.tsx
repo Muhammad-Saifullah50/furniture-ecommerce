f95cd252a0c05ec928819966264d63f061c21d05
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-10 11:01:34 +0500

    added suppress hydartion warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
 
   const session = await getServerSession(authOptions)
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className='font-poppins'>
         <Provider session={session}>
           <Navbar />
