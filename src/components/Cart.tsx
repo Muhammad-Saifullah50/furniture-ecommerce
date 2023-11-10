6d0b1d3ff58548621af13808e037623b885714b7
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-10 18:41:57 +0500

    useeffect again

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -17,54 +17,63 @@ interface CartItems {
 }
 
 const Cart = () => {
-    const [cartItems, setCartItems] = useState<CartItems[]>([])
     const session = useSession()
- 
-}
+    const [cartItems, setCartItems] = useState<CartItems[]>([])
 
-const totalPrice = cartItems.reduce((total, item) => {
-    //@ts-ignore
-    return total + parseFloat(item.price) * item.quantity
-}, 0)
-return (
-    <div>
-        <Sheet>
-            <SheetTrigger>
-                <Image
-                    src="/cart.svg"
-                    height={27}
-                    alt='icon'
-                    width={27}
+    useEffect(() => {
+        const getData = async () => {
+            //@ts-ignore
+            const items = await getUserCartItems(session.data?.user?.id)
+            setCartItems(items)
+            
+        }
+        getData()
+    }, [])
 
-                />
-                <div className='bg-red-700 absolute top-3 text-white rounded-full px-1 right-9 text-[10px]'>{cartItems.length}</div>
-            </SheetTrigger>
-            <SheetContent className='px-6'>
-                <SheetHeader>
-                    <SheetTitle>Your Shopping Cart</SheetTitle>
-                    {cartItems.map((item: any) => (
-                        <CartItem
-                            name={item.name}
-                            price={item.price}
-                            image={item.image}
-                            quantity={item.quantity}
-                            key={item.id}
-                            itemId={item.id}
-                        />
-                    ))}
-                </SheetHeader>
-                <SheetFooter >
-                    <div className='flex flex-col items-center w-full font-semibold gap-8 mt-8'>
-                        <p> Total Amount: <span className='text-gold-primary'>Rs {totalPrice}</span></p>
-                        <Button className='w-full text-base font-semibold'>Checkout</Button>
-                    </div>
 
-                </SheetFooter>
-            </SheetContent>
-        </Sheet>
+    const totalPrice = cartItems.reduce((total, item) => {
+        //@ts-ignore
+        return total + parseFloat(item.price) * item.quantity
+    }, 0)
+    return (
+        <div>
+            <Sheet>
+                <SheetTrigger>
+                    <Image
+                        src="/cart.svg"
+                        height={27}
+                        alt='icon'
+                        width={27}
 
-    </div >
-)
+                    />
+                    <div className='bg-red-700 absolute top-3 text-white rounded-full px-1 right-9 text-[10px]'>{cartItems.length}</div>
+                </SheetTrigger>
+                <SheetContent className='px-6'>
+                    <SheetHeader>
+                        <SheetTitle>Your Shopping Cart</SheetTitle>
+                        {cartItems.map((item: any) => (
+                            <CartItem
+                                name={item.name}
+                                price={item.price}
+                                image={item.image}
+                                quantity={item.quantity}
+                                key={item.id}
+                                itemId={item.id}
+                            />
+                        ))}
+                    </SheetHeader>
+                    <SheetFooter >
+                        <div className='flex flex-col items-center w-full font-semibold gap-8 mt-8'>
+                            <p> Total Amount: <span className='text-gold-primary'>Rs {totalPrice}</span></p>
+                            <Button className='w-full text-base font-semibold'>Checkout</Button>
+                        </div>
+
+                    </SheetFooter>
+                </SheetContent>
+            </Sheet>
+
+        </div >
+    )
 }
 
 export default Cart
